c35ef78e43d5ec1718469527b0cbe7fcc5a4839a
Anhtt9x <anhtt454598@gmail.com>
2024-05-19 17:21:44 +0700

    update_ver_1

--- example.py
+++ example.py
@@ -55,7 +55,10 @@ if __name__ == "__main__":
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    
+    remove_uri = 'https://dagshub.com/Anhtt9x/MLflow-Basic.mlflow'
+    mlflow.set_tracking_uri(remove_uri)
+
+
 
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
@@ -76,11 +79,9 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        remove_uri = 'https://dagshub.com/Anhtt9x/MLflow-Basic.mlflow'
-        mlflow.set_tracking_uri(remove_uri)
+        
 
-        predictions = lr.predict(train_x)
-        signature = infer_signature(train_x, predictions)
+
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
@@ -91,7 +92,7 @@ if __name__ == "__main__":
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
             mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="ElasticnetWineModel", signature=signature
+                lr, "model", registered_model_name="ElasticnetWineModel"
             )
         else:
-            mlflow.sklearn.log_model(lr, "model", signature=signature)
+            mlflow.sklearn.log_model(lr, "model")
