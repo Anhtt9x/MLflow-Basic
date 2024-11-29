0fdf92de9a3ca43f07eb287e2ec4fb0762b0bf77
Anhtt9x <anhtt454598@gmail.com>
2024-11-29 14:11:32 +0700

    final

--- example.py
+++ example.py
@@ -35,7 +35,41 @@ if __name__ == "__main__":
     except Exception as e:
         logger.exception("Unable to download training & test CSV, check url: %s", e)
 
-    train, test = train_test_split(data, test_size=0.2, random_state=42)
+    train, test = train_test_split(data, test_size=0.25, random_state=42)
     X_train, y_train = train.drop(["quality"], axis=1), train[["quality"]]
     X_test, y_test = test.drop(["quality"], axis=1), test[["quality"]]
     
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    with mlflow.start_run():
+
+        model = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        rmse, mae, r2 = eval_metrics(y_test, y_pred)
+
+        print(f"Elasticnet model (alpha={alpha}, l1_ratio={l1_ratio}):")
+        print(f"RMSE: {rmse:.3f}, MAE: {mae:.3f}, R2: {r2:.3f}")
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
+
+        predictions = model.predict(X_train)
+        signature = infer_signature(X_train, predictions)
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        # Model registry does not work with file store
+        if tracking_url_type_store != "file":
+            # Register the model
+            # There are other ways to use the Model Registry, which depends on the use case,
+            # please refer to the doc for more information:
+            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            mlflow.sklearn.log_model(
+                model, "model", registered_model_name="ElasticnetWineModel", signature=signature
+            )
+        else:
+            mlflow.sklearn.log_model(model, "model", signature=signature)
